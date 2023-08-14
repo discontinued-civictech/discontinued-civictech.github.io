7b2b399b0000962940a4114b391d140ba1a5bc1c
discontinued-civictech <80161688+discontinued-civictech@users.noreply.github.com>
2023-08-14 05:07:08 +0000

    update cookie consent.html and ga.js

--- _includes/ga.js
+++ _includes/ga.js
@@ -1,9 +1,17 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-6MM46DN2JB"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+/*This function will load script and call the callback once the script has loaded*/
 
-  gtag('config', 'G-6MM46DN2JB');
-</script>
+function loadScriptAsync(scriptSrc, callback) {
+  if (typeof callback !== 'function') {
+      throw new Error('Not a valid callback for async script load');
+  }
+  var script = document.createElement('script');
+  script.onload = callback;
+  script.src = scriptSrc;
+  document.head.appendChild(script); }
+  
+  loadScriptAsync("https://www.googletagmanager.com/gtag/js?id=G-6MM46DN2JB", function () {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-6MM46DN2JB', { 'anonymize_ip': true }); })
+  
